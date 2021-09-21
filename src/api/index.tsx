70d93c9b7224743f27a4c719621f4e229f31c4ed
sstojanovic <sstojanovic@transperfect.com>
2021-09-21 21:39:37 +0200

    update

--- src/api/index.tsx
+++ src/api/index.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL : 'https://car-rental-330ee-default-rtdb.europe-west1.firebasedatabase.app'
+  baseURL : `${process.env.REACT_APP_BASE_URL}`
 });
 
 export default instance;
